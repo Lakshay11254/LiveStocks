9683d2bd903077779914e67fe41fd78f5ac0c293
Lakshay Malhotra <68064745+Lakshay11254@users.noreply.github.com>
2023-01-31 14:10:57 +0000

    edited fetched api

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import finnHub from "../apis/finnHub"
 
 export const StockList = () => {
-  const [stock, setStock] = useState()
+  const [stock, setStock] = useState([])
   const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
 
   useEffect(() => {
@@ -10,24 +10,30 @@ export const StockList = () => {
     const fetchData = async () => {
       const responses = []
       try {
-        const response1 = await finnHub.get("/quote", {
-          params: {
-            symbol: "GOOGL"
-          }
-        })
-        responses.push(response1)
-        const response2 = await finnHub.get("/quote", {
-          params: {
-            symbol: "MSFT"
-          }
-        })
-        responses.push(response2)
-        const response3 = await finnHub.get("/quote", {
-          params: {
-            symbol: "AMZN"
-          }
-        })
-        responses.push(response3)
+  
+        const responses = await Promise.all(watchList.map((stock) => {
+          return finnHub.get("/quote",{
+            params: {
+              symbol: stock
+            }
+          })
+        }))
+        //   finnHub.get("/quote", {
+        //   params: {
+        //     symbol: "GOOGL"
+        //   }
+        // }),finnHub.get("/quote", {
+        //   params: {
+        //     symbol: "MSFT"
+        //   }
+        // }),finnHub.get("/quote", {
+        //   params: {
+        //     symbol: "AMZN"
+        //   }
+        // }))
+        
+        
+        // responses.push(response3)
 
         console.log(responses)
         if (isMounted) {

1a33dad6846e701a8c027ee1c2dcc83bc14c7854
Lakshay Malhotra <68064745+Lakshay11254@users.noreply.github.com>
2023-02-01 11:07:58 +0000

    got our stocks data!!

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,18 +1,27 @@
 import { useState, useEffect } from "react"
 import finnHub from "../apis/finnHub"
+import { BsFillCaretDownFill } from "react-icons/bs"
+import { BsFillCaretUpFill } from "react-icons/bs"
 
 export const StockList = () => {
   const [stock, setStock] = useState([])
   const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
+
+  const changeColor = (change) => {
+    return change > 0 ? "success" : "danger"
+  }
+  const renderIcon = (change) => {
+    return change > 0 ? <BsFillCaretUpFill /> : <BsFillCaretDownFill />
+  }
 
   useEffect(() => {
     let isMounted = true
     const fetchData = async () => {
       const responses = []
       try {
-  
+
         const responses = await Promise.all(watchList.map((stock) => {
-          return finnHub.get("/quote",{
+          return finnHub.get("/quote", {
             params: {
               symbol: stock
             }
@@ -31,13 +40,23 @@ export const StockList = () => {
         //     symbol: "AMZN"
         //   }
         // }))
-        
-        
+
+
         // responses.push(response3)
 
         console.log(responses)
+
+        const data = responses.map((response) => {
+
+          return {
+            data: response.data,
+            symbol: response.config.params.symbol
+
+          }
+        })
+
         if (isMounted) {
-          setStock(responses)
+          setStock(data)
         }
 
       } catch (err) {
@@ -50,5 +69,36 @@ export const StockList = () => {
     return () => (isMounted = false)
   }, [])
 
-  return <div>StockList</div>
+  return <div>
+    <table className="table hover mt-5">
+      <thead style={{ color: "rgb(79,89,102)" }}>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Last</th>
+          <th scope="col">Chg</th>
+          <th scope="col">Chg%</th>
+          <th scope="col">High</th>
+          <th scope="col">Low</th>
+          <th scope="col">Open</th>
+          <th scope="col">Pclose</th>
+        </tr>
+      </thead>
+      <tbody>
+        {stock.map((stockData) => {
+          return (
+            <tr className="table-row" key={stockData.symbol}>
+              <th scope="row"> {stockData.symbol}</th>
+              <td>{stockData.data.c}</td>
+              <td className={`text-${changeColor(stockData.data.d)}`}>{stockData.data.d}  {renderIcon(stockData.data.d)}</td>
+              <td className={`text-${changeColor(stockData.data.d)}`}>{stockData.data.dp} {renderIcon(stockData.data.d)}</td>
+              <td>{stockData.data.h}</td>
+              <td>{stockData.data.l}</td>
+              <td>{stockData.data.o}</td>
+              <td>{stockData.data.pc}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  </div>
 }
